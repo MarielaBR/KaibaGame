46b360fcfe38926c8845f1f6c19e7b3dbb977e5d
amaral123 <juegos_games3@hotmail.com>
2016-05-26 00:12:41 -0600

    commit intneto2

--- app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Login.java
+++ app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Login.java
@@ -1,5 +1,7 @@
 package ittepic.edu.mx.tpdm_kaiba;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +9,15 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Login extends AppCompatActivity {
     Button entrar,registro;
+    EditText usuario,contrasena;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,8 @@ public class Login extends AppCompatActivity {
 
         entrar = (Button)findViewById(R.id.button);
         registro = (Button)findViewById(R.id.button4);
+        usuario=(EditText)findViewById(R.id.editText);
+        contrasena=(EditText)findViewById(R.id.editText2);
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +45,45 @@ public class Login extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Conexion web= new Conexion(Login.this);
+                    web.agregarVariables("usuario",usuario.getText().toString());
+                    web.agregarVariables("contrasena",contrasena.getText().toString());
+
+                    web.execute(new URL("kaiba.esy.es/login.php"));
+                }catch(MalformedURLException e){
+                    AlertDialog.Builder  alerta= new AlertDialog.Builder(Login.this);
+                    alerta.setTitle("ERROR").setMessage(e.getMessage()).show();
+                }
+            }
+        });
+    }
+
+    public void mostrarResultado(String resultado){
+        AlertDialog.Builder alerta= new AlertDialog.Builder(this);
+
+        if(resultado.startsWith("Error_404_1")){
+            resultado="Hosting no encontrado";
+        }
+        if(resultado.startsWith("Error_404_2")){
+            resultado="Error al ENVIAR/RECIBIR informacion con el PHP";
+        }
+        if(resultado.startsWith("Error_404")){
+            resultado="Al parecer no existe el PHP buscado";
+        }
+        alerta.setTitle("Respuesta desde SERVIDOR:")
+                .setMessage(resultado)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
--- app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Principal.java
+++ app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Principal.java
@@ -28,5 +28,6 @@ public class Principal extends AppCompatActivity {
             }
         });
     }
-    //prueba1
+
+
 }

f3513a42508060afdab971b3441639b9e19a8e59
Bearnal19 <cesar_enigma19@hotmail.com>
2016-05-28 00:33:53 -0600

    LoginNT

--- app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Principal.java
+++ app/src/main/java/ittepic/edu/mx/tpdm_kaiba/Principal.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.content.Intent;
 
 public class Principal extends AppCompatActivity {
     Button continuar; //hol
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +19,9 @@ public class Principal extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_principal);
 
+        img = (ImageView)findViewById(R.id.imageView2);
 
-        continuar = (Button)findViewById(R.id.button2);
-
-        continuar.setOnClickListener(new View.OnClickListener() {
+        img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Principal.this, Login.class );

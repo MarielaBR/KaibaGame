7e4a6f2ec0aca755d36c11f8848ea50196c64dc8
Mariea <rika.furude8@gmail.com>
2016-05-29 17:04:24 -0600

    imagenes

--- app/src/main/java/ittepic/edu/mx/tpdm_kaiba/FragmentoPerfil.java
+++ app/src/main/java/ittepic/edu/mx/tpdm_kaiba/FragmentoPerfil.java
@@ -1,11 +1,15 @@
 package ittepic.edu.mx.tpdm_kaiba;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by MARIELA on 28/05/2016.
@@ -18,5 +22,35 @@ public class FragmentoPerfil extends Fragment{
 
         return root;
 
+    }
+
+    public void mostrarResultado(String resultado){
+        AlertDialog.Builder alerta= new AlertDialog.Builder(getActivity());
+
+
+        if(resultado.startsWith("CONFIRMADO")){
+            Toast.makeText(getActivity(), "Su cuenta ha sido confirmada", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(getActivity(),Login.class );
+            startActivity(i);
+        }
+        else{
+            if(resultado.startsWith("ERROR2")){
+                resultado="Error al enviar el codigo";
+            }
+
+            if(resultado.startsWith("INCORRECTO")){
+                resultado="Código incorrecto, vuelva a intentarlo";
+            }
+
+            alerta.setTitle("ERROR")
+                    .setMessage(resultado)
+                    .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+        }
     }
 }
